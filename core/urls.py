39f0f4e645a00d2bac0a7672cdf2eeca4df75c7b
Valijonov Bahodirjon Baxtiyor o'g'li <BValizhonov@beeline.uz>
2026-07-05 21:45:12 +0500

    fixed admin panel url routing and nginx configuratin file for admin routes

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("superadmin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/", include("apps.api_router")),
